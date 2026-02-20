21e35b86a49f58969bf1df48fc9404696deb19de
gamer96shubham96-star <gamer96shubham96@gmail.com>
2026-02-20 17:46:36 +0530

    w

--- script.js
+++ script.js
@@ -42,12 +42,16 @@ window.onload = function () {
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      entry.target.classList.add('show');
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
     }
   });
-}, { threshold: 0.15 });
+}, {
+  threshold: 0.2
+});
 
-document.querySelectorAll('.fade-up').forEach(el => {
+document.querySelectorAll(".animate").forEach(el => {
   observer.observe(el);
 });
 
