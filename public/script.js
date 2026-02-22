4a18b2355aad2cc7f9c402ec47d83e4b760d8c8d
gamer96shubham96-star <gamer96shubham96@gmail.com>
2026-02-22 11:20:37 +0530

    1

--- public/script.js
+++ public/script.js
@@ -74,6 +74,14 @@ window.addEventListener("scroll", () => {
 });
 
 
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader");
+  setTimeout(() => {
+    loader.classList.add("hidden");
+  }, 2000);
+});
+
+
 // ========================================
 // COUNTER ANIMATION
 // ========================================
